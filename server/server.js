d5ce1a6ae031ebc05dc75428889e291bbbeee93e
danielzhao <danielzhaozd@gmail.com>
2016-07-29 17:50:47 -0400

    why

--- server/server.js
+++ server/server.js
@@ -1,1 +1,21 @@
-server.js
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+// app.use(express.static(__dirname + "/client"));
+app.use(express.static(__dirname + "/node_modules"));
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log('Server is running');
+});
+
+app.get('/fuckDan', function(req, res){
+  console.log('request received at /fuckDan');
+  res.send(JSON.stringify({"message": "Fuck Dan"}));
+});
