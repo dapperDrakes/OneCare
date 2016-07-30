1fee7ac234dae8c4c41eecb3e1680ee1664259d5
Kenton <kentonlin11@gmail.com>
2016-07-30 16:36:17 -0400

    update db

--- db/dbhelper.js
+++ db/dbhelper.js
@@ -1,5 +1,5 @@
 var Model = require('./db.js');
-
+var jwt  = require('jwt-simple');
 var dbFunc = {
 
 	addScript: function(script, res) {
@@ -14,7 +14,7 @@ var dbFunc = {
 		})
 	},
 
-	getScripts: function(scriptName, res) {
+	getScripts: function(scriptName, user, res) {
 		Model.script.findOne({'name':scriptName},function (err, found) {
 			if(err){
 				console.log('error in fetching tasks'); 
@@ -22,9 +22,82 @@ var dbFunc = {
 			console.log(found);
 			res.send(found); 
 		});
+	},
+
+/* AUTHENTICATION FUNCTIONS */
+
+	signup: function(newUser, res, next) {
+		Model.user.find({"username": newUser.username}, function(err, user){
+			if(err) {
+				console.log("Error: ", error);
+			}
+			if(!user.length) { //if a user is not found, an empty array is returned
+				console.log("user does NOT already exist");
+				var user = new Model.user(newUser);
+				user.save(function(err){
+					if(err) {
+						console.log("new user not saved", err);
+					}
+				console.log("new user saved");
+				var token = jwt.encode(user, 'secret'); //create new token
+        res.json({"token": token, "user": {"id": user._id, "username": user.username}}); //send new token and user object
+			})
+
+			}
+			else {
+				console.log("user already exists: ", user);
+				next(new Error("user already exists"));
+			}
+		})
+	},
+
+	signin: function(reqUser, res, next){
+		Model.user.find({"username": reqUser.username}, function(err, user){
+			if(err){ //if error in query
+				next("Error: ", error);
+			}
+			if(!user.length){ //if user not found
+				next(new Error("username does not exist"));
+			}
+			else{ //if user found
+				console.log("*** this is the user", user)
+				user.comparePassword(reqUser.password, function(err, isMatch){
+					if(err) {throw err;}
+					if(!isMatch){
+						next(new Error("Incorrect password")) //will send an error if incorrect password
+					}
+					else{
+						console.log("password correct!");
+						var token = jwt.encode(user[0], 'secret'); //create new token
+            res.json({"token": token, "user": {"id": user[0]._id, "username": user[0].username}}); //send new token and user object
+					}
+				})
+			}
+		})
+	},
+
+	checkAuth: function(req, res, next){
+		var token = req.headers['x-access-token'];
+    if (!token) {
+      next(new Error('No token'));
+    }
+    else {
+      var user = jwt.decode(token, 'secret');
+      console.log("Decoded user:", user);
+      Model.user.find(user, function(err, user){
+      	if(err){
+      		next("Error: ", error);
+      	}
+      	if(!user.length){ //user not found
+      		res.status(401).send();
+      	}
+      	else{ //token decoded and user found in database
+      		console.log("user authenticated")
+      		res.status(200).send();
+      	}
+      });
+    }
 	}
-
-
 
 
 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var dbHelpers = require('../db/dbhelper.js');
+var bodyParser = require('body-parser');
 var app = express();
 
 
@@ -21,13 +21,25 @@ app.get('/fuckDan', function(req, res){
   console.log('request received at /fuckDan');
   res.send(JSON.stringify({"message": "Fuck Dan"}));
 });
-
+//add a script to the script collection 
 app.post('/api/script/add', function(req, res) {
 	var newScript = req.body; 
 	dbHelpers.addScript(newScript, res); 
 }); 
-
+// locate a script will probably need to contain a username and scriptname optionally dosage
 app.post('/api/script/find', function(req, res) {
 	var findScript = req.body; 
 	dbHelpers.getScripts( findScript, res); 
 }); 
+
+
+//signup
+app.post('/api/signup', function(req, res, next) {
+  var userSignup = req.body; 
+  dbHelpers.signup(userSignup, res, next); 
+});
+
+app.post('/api/signin', function(req, res) {
+  var userSignin = req.body;
+  dbHelpers.signin(userSignin, res, next); 
+})
